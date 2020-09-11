8f8beb88c1610c348eabb03788d0829c05af3047
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-09-11 14:06:43 +0900

    [update] テキストファイルの読み込み

--- VivConverter/VivConverter/Form1.cs
+++ VivConverter/VivConverter/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,22 @@ namespace VivConverter
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e) //開くボタンの処理
+        {
+            DialogResult diar = openFileDialog1.ShowDialog();
+            if (diar == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //変換ボタンの処理
+        {
+            StreamReader strr = new StreamReader(textBox1.Text, Encoding.GetEncoding("UTF-8"));
+
+            string viv = strr.ReadToEnd();
+            strr.Close();
+            label1.Text = viv;
+        }
     }
 }
